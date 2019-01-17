781f70f49b0b3cae9a6f3d46497277b7d6e61bab
Princess413 <35474633+Princess413@users.noreply.github.com>
2019-01-17 22:43:52 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Natura Parents</title>
+</head>
+<body>
+<img src="usecase.JPG">
+	<p>The user or adopter calls on the orphanage to verify the child that it wants to adopt meanwhile the orphanage 
+  after accomplishing all the process needed will connect to the admin to give information regarding the adoption
+  Admin verifies the applications of the adopters in search of adoptee.
+	</p>
+</body>
+</html>
