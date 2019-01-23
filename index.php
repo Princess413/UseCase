59581485a810308505a5ea5d2934b6047ec50f66
Princess413 <35474633+Princess413@users.noreply.github.com>
2019-01-23 13:46:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 </head>
 <body>
 <img src="usecasedia.jpg">
-	<p></p>
+	<p>This Use Case Diagram involves two actors, the admin who manages the application and the adopter acts as the user.
+	Adopter log ins and the admin verifies it, if no record exist error will display that will lead you to sign up to have access.
+	After signing in, user looks for an orphanage to search for an adoptee. When adopter made up his/her mind it will proceed to
+	adoption process in person.</p>
 </body>
 </html>
