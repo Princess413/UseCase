b9dcb2b5e6b70f0305840b48b56502767bf425b2
Princess413 <35474633+Princess413@users.noreply.github.com>
2019-01-23 12:12:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
 	<title>Use Case</title>
 </head>
 <body>
-<img src="usecase.JPG">
-	<p>The user or adopter calls on the orphanage to verify the child that it wants to adopt meanwhile the orphanage 
-  after accomplishing all the process needed will connect to the admin to give information regarding the adoption
-  Admin verifies the applications of the adopters in search of adoptee.
-	</p>
+<img src="usecasedia.jpg">
+	<p></p>
 </body>
 </html>
